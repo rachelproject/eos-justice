14c08ab984d29583e84c9472da259260b2a59d6c
Jeremy Schwartz <jeremy@worldpossible.org>
2016-12-02 13:13:01 -0800

    Update eos.sh

--- eos.sh
+++ eos.sh
@@ -1,2 +1,30 @@
 #remove facebook icon
 gsettings set org.gnome.shell enable-social-bar false
+
+#remove following applications
+Atomic Tanks
+Battleships
+Battle Tanks
+Celebrities
+Chromium
+Cooking
+Encyclopedia
+KillBots
+Kobo Deluxe
+MARS
+Maternity
+Megaquest
+Open Arena
+Warmux
+Warzone
+Wesnoth
+YouVideos
+
+#remove shared user account
+#create 'user' user and allow that user to set the password on first login
+
+#figure out a way to install the serverless RACHEL-USB.  Would also like to add to that static content, RACHEL modules:
+en-saylor
+en-oya
+en-boundless
+#I just want this rsync command to be automated, and RACHEL to put in a folder that is READ-ONLY for users
