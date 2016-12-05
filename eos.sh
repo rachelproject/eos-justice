7bf086268bed421fe0d85e8acff0a9fcc51e975b
Jeremy Schwartz <jeremy@worldpossible.org>
2016-12-05 07:42:13 -0800

    Update eos.sh

--- eos.sh
+++ eos.sh
@@ -1,32 +1,79 @@
-#remove facebook icon
-gsettings set org.gnome.shell enable-social-bar false
+#!/bin/bash
 
-#remove following applications
-Atomic Tanks
-Battleships
-Battle Tanks
-Celebrities
-Chromium
-Cooking
-Encyclopedia
-KillBots
-Kobo Deluxe
-MARS
-Maternity
-Megaquest
-Open Arena
-Warmux
-Warzone
-Wesnoth
-YouVideos
+APPS_TO_REMOVE="org.kde.Knavalbattle net.sourceforge.Atanks net.sourceforge.Btanks \
+com.endlessm.celebrities.en com.endlessm.cooking.en com.endlessm.encyclopedia.en \
+org.kde.Killbots net.olofson.Kobodeluxe org.marsshooter.Marsshooter com.endlessm.maternity.en \
+org.megaglest.Megaglest org.openarena.Openarena net.sourceforge.Warmux net.wz2100.Warzone2100 \
+org.wesnoth.Wesnoth"
 
-#remove shared user account
-#create 'user' user and allow that user to set the password on first login
+APPS_TO_INSTALL="com.google.Chrome"
 
-#figure out a way to install the serverless RACHEL-USB.  Would also like to add to that static content, RACHEL modules:
-en-saylor
-en-oya
-en-boundless
-#I just want this rsync command to be automated, and RACHEL to put in a folder that is READ-ONLY for users
+REMOVE_USER="shared"
+ADD_USER="user"
 
-#install chrome - instructions on community.endlessm.com
+function delete_applications {
+    echo "Starting applications deleting"
+    for APP in $APPS_TO_REMOVE; do
+        if `flatpak uninstall $APP`; then
+            echo "$APP has been removed"
+        else
+            echo "Deleting $APP failed"
+        fi
+    done
+    echo "Applications deleting finished"
+}
+
+function install_applications {
+    echo "Starting applications installiation"
+    for APP in $APPS_TO_INSTALL; do
+        if `flatpak install eos-apps $APP eos3.0`; then
+            echo "$APP has been installed"
+        else
+            echo "$APP installation failed"
+        fi
+    done
+    echo "Applications installation finished"   
+}
+
+function delete_user {
+    echo "Starting to delete $REMOVE_USER user"
+    if `userdel $REMOVE_USER`; then
+        echo "User $REMOVE_USER has been successfully deleted"
+    else
+        echo "Deleting $REMOVE_USER user failed"
+    fi
+}
+
+function create_user {
+    echo "Starting to create new user"
+    if `useradd -c "$ADD_USER" -m -s "/bin/bash" $ADD_USER`; then
+        echo "User has been created"
+        echo $ADD_USER:"123456" | chpasswd
+        chage -d 0 $ADD_USER
+    else
+        echo "User adding failed"
+    fi
+}
+
+function disable_social_bar {
+    echo "Disabling social bar"
+    if `gsettings set org.gnome.shell enable-social-bar false`; then
+        echo "Social bar has been disabled"
+    else
+        echo "Social bar disabling failed"
+    fi
+}
+
+function check_if_root {
+    if (( $EUID != 0 )); then
+        echo "Please run as root"
+        exit 1
+    fi
+}
+
+check_if_root
+disable_social_bar
+delete_applications
+install_applications
+delete_user
+create_user
