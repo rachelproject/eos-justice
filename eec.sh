915ef1000729e800e542c3b4366e00d262a97f95
Jeremy Schwartz <jeremy@worldpossible.org>
2020-02-04 14:38:36 -0800

    Update eec.sh

--- eec.sh
+++ eec.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DATA_PATH="/var/local/bin/rachel/eec/data"
+EXTRAS_PATH="/var/local/bin/rachel/eec/eec-extras"
 # Stop the containers if they are running
 sudo podman stop eec-http 2> /dev/null
 sudo podman stop eec-mysql 2> /dev/null
@@ -14,10 +16,19 @@ set -e
 # Note: This is work around to the following bug, in the future this might not be necessary
 # https://github.com/containers/libpod/issues/3759
 sudo podman pod create --publish 9006:80 --name eec
-sudo podman create --pod eec --name eec-mysql --mount
-type=bind,src=/var/local/bin/rachel/modules/eec,dst=/var/lib/mysql -e MYSQL_ROOT_PASSWORD=QCBDLNa6 -e
-MYSQL_DATABASE=eec -e MYSQL_USER=eec -e MYSQL_PASSWORD=EbJEBWmE eec-mysql:5.0.0.5
-sudo podman create --pod eec --name eec-http eec-http:5.0.0.5-endlessos
+sudo podman create --pod eec --name eec-mysql \
+ --mount type=bind,src=$DATA_PATH,dst=/var/lib/mysql \
+ -e MYSQL_ROOT_PASSWORD=QCBDLNa6 \
+ -e MYSQL_DATABASE=eec \
+ -e MYSQL_USER=eec \
+ -e MYSQL_PASSWORD=EbJEBWmE \
+ eec-mysql:5.0.0.5
+sudo podman create --pod eec --name eec-http \
+ --mount type=bind,src=$EXTRAS_PATH/lessons,dst=/var/www/eec/public/lessons \
+ eec-http:5.0.0.5-endlessos.2
 # Start the containers
 sudo podman start eec-mysql
 sudo podman start eec-http
+# Fix file permissions
+sudo podman exec eec-http chown www-data:www-data -R /var/www/eec/public/lessons
+
